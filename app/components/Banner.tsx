dee753b78f1c146eb4818d57987b1052898c3d4e
aayushgurung <gurung087@gmail.com>
2023-09-27 14:56:56 +0545

    Ai  Tools Home page

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import Image from 'next/image'
-const Banner = () => {
+
+interface BannerProps {
+  title:string,
+  description:string
+}
+const Banner: React.FC<BannerProps> = ({title,description}) => {
   return (
     <>
     <section
-    className="bg-indigo-600 hover:bg-indigo-700 mt-2 pt-14 pb-14 shadow-lg text-white relative"
+    className="bg-indigo-600 mt-2 pt-14 pb-14 shadow-lg text-white relative"
   >
     
     <div className="container mx-auto px-20 relative">
-      <h1 className="text-4xl md:text-5xl font-bold">Image Enhancer</h1>
-      <p className="mt-4 z-10 relative">
-        The Image Enhancer is a powerful AI tool that intelligently enhances
-        the quality and appearance of images. Using advanced algorithms, it
-        can improve sharpness, color vibrancy, and overall visual clarity,
-        making images look their best. Whether it's for photography, design,
-        or any application, the Image Enhancer effortlessly transforms
-        ordinary images into stunning, high-quality visuals with just a click.
+      <h1 className="text-4xl md:text-5xl font-bold">{title}</h1>
+      <p className="mt-4 z-10 relative text-justify">
+        {description}
       </p>
     </div>
     <Image
--- app/image/image-enhancer/page.tsx
+++ app/image/image-enhancer/page.tsx
@@ -1,11 +1,25 @@
 import Banner from "@/app/components/Banner";
 import InputOutput from "@/app/components/InputOutput";
 
+const bannerData = {
+  title: "Face Enhancer",
+  description: `The Image Enhancer is a powerful AI tool 
+  that intelligently enhances the quality and appearance of images.
+   Using advanced algorithms, it can improve sharpness, color vibrancy, 
+   and overall visual clarity, making images look their best. Whether
+    it's for photography, design, or any application, the Image Enhancer
+     effortlessly transforms ordinary images into stunning, high-quality
+      visuals with just a click.`,
+
+};
+
 export default function Home() {
   return (
     <>
-    <Banner />
-    <InputOutput/>
+      <Banner
+        {...bannerData}
+      />
+      <InputOutput />
     </>
-  )
+  );
 }
